d232f042bfb701463717507af5fba31df7d96ce6
endi69 <endidervishaliaj@gmail.com>
2026-09-23 01:54:32 +0300

    SmartCoach v2: add sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='smartcoach-v2.0.0';
+const CORE=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./',c));return r}).catch(()=>caches.match('./')));
+    return;
+  }
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
+});
